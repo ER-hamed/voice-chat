07d68ee93b3d6b0ff46497e3c980601b05f63e53
ER-hamed <46535606+ER-hamed@users.noreply.github.com>
2022-01-04 19:50:56 +0330

    Add files via upload

--- fwc.py
+++ fwc.py
@@ -1,20 +1,32 @@
 #! /usr/bin/python3
 
-from os import system
+from os import popen
 
 while True:
+    popen('clear')
+    popen('sudo iptables -L --line-numbers')
     command = input('Command: ').split(' ')
     if command == ['']:
         exit()
+    elif command[0] == 'set':
+        popen('sudo iptables -D INPUT -j DROP')
+        popen('sudo iptables -D INPUT -p tcp --dport 22 -j ACCEPT')
+        popen('sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT')
+        popen('sudo iptables -A INPUT -j DROP')
     elif command[0] == 'open' or command[0] == 'o':
-        system('sudo iptables -D INPUT -j DROP')
-        system('sudo iptables -A INPUT -p tcp --dport ' + command[1] + ' -j ACCEPT')
-        system('sudo iptables -A INPUT -j DROP')
+        popen('sudo iptables -D INPUT -j DROP')
+        popen('sudo iptables -D INPUT -p tcp --dport ' + command[1] + ' -j DROP')
+        popen('sudo iptables -D INPUT -p tcp --dport ' + command[1] + ' -j ACCEPT')
+        popen('sudo iptables -A INPUT -p tcp --dport ' + command[1] + ' -j ACCEPT')
+        popen('sudo iptables -A INPUT -j DROP')
     elif command[0] == 'close' or command[0] == 'c':
-        system('sudo iptables -D INPUT -j DROP')
-        system('sudo iptables -A INPUT -p tcp --dport ' + command[1] + ' -j DROP')
-        system('sudo iptables -A INPUT -j DROP')
-    elif command[0] == 'show':
-        system('sudo iptables -L --line-numbers')
+        popen('sudo iptables -D INPUT -j DROP')
+        popen('sudo iptables -D INPUT -p tcp --dport ' + command[1] + ' -j ACCEPT')
+        popen('sudo iptables -D INPUT -p tcp --dport ' + command[1] + ' -j DROP')
+        popen('sudo iptables -A INPUT -p tcp --dport ' + command[1] + ' -j DROP')
+        popen('sudo iptables -A INPUT -j DROP')
+    elif command[0] == 'remove' or command[0] == 'r':
+        popen('sudo iptables -D INPUT -p tcp --dport ' + command[1] + ' -j ACCEPT')
+        popen('sudo iptables -D INPUT -p tcp --dport ' + command[1] + ' -j DROP')
     else:
         print('Command not found')
